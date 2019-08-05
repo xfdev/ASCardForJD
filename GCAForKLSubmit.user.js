b05b32bd3c2b1510006767c680ecb7f935e5e912
xiao fang <anangcocoy@gmail.com>
2019-08-05 17:51:43 +0800

    Update GCAForKLSubmit.user.js

--- GCAForKLSubmit.user.js
+++ GCAForKLSubmit.user.js
@@ -11,24 +11,26 @@
 
 (function() {
     'use strict';
-    // 屏蔽错误日志 -url:https://analytics.163.com/ntes -url:https://rev.da.netease.com/
+	// 屏蔽错误日志 -url:https://analytics.163.com/ntes -url:https://rev.da.netease.com/
     // 金额元素
-    var moneyLabel = document.getElementsByClassName("j-total")[0];
-    moneyLabel.addEventListener('DOMNodeInserted', function(e) {
-        var ttboxDiv = document.getElementsByClassName("ttbox")[0];
-        // 面值
+	var moneyLabel = document.getElementsByClassName("j-total")[0];
+	moneyLabel.addEventListener('DOMNodeInserted', function(e) {
+		var ttboxDiv = document.getElementsByClassName("ttbox")[0];
+		// 面值
         var parValue = ttboxDiv.getElementsByTagName("b")[0].innerText;
         // 数量
         var num = ttboxDiv.getElementsByTagName("b")[1].innerText;
         // 金额
         var money = document.getElementsByClassName("j-total")[0].innerText;
         if (money <= parValue*num*0.9) {
-            // 提交
-            document.getElementById("submitbtn").click();
-            console.log("已提交");
+        	// 提交
+        	document.getElementById("submitbtn").click();
+        	console.log("已提交");
         } else {
-            console.log("没打折，去刷新："+money);
-            setTimeout('window.location.reload()', 500);
+        	console.log("没打折，去刷新："+money);
+            //setTimeout('window.location.reload()', 500);
+            window.location.reload();
         }
     });
+    console.log("已绑定");
 })();
